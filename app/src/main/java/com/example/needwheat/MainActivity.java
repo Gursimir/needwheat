29115881e95d13eec1dc6fec203f172fac8ebe80
Gursimir <kgursimran648@gmail.com>
2020-11-13 22:54:24 +0530

    commit

--- app/src/main/java/com/example/needwheat/MainActivity.java
+++ app/src/main/java/com/example/needwheat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.needwheat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView1,textView2,textView3,textView4;
     ImageView imageView,imageView1,imageView2,imageView3,imageView4;
     EditText editText;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,52 +38,40 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.sendamount);
 
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String amount = editText.getText().toString();
-                textView1.setText("Could you please pack me "+amount+" kg of wheat flour.");
-                imageView1.setImageResource(R.drawable.female_avatar);
+        imageView.setOnClickListener(v -> {
+            String amount = editText.getText().toString();
+            textView1.setText("Could you please pack me "+amount+" kg of wheat flour.");
+            imageView1.setImageResource(R.drawable.female_avatar);
 
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageView2.setImageResource(R.drawable.male_avatar);
-                        textView2.setText("Yes ma'am! wait a minute.");
-                    }
-                },1000);
-                int flourAmount = Integer.parseInt(amount),count3 = 0,count5 = 0;
-                while (flourAmount>=5 || flourAmount == 3){
-                    if(flourAmount == 3 || flourAmount == 6 || flourAmount == 9){
-                        flourAmount = flourAmount-3;
-                        count3++;
-                    }else{
-                        flourAmount = flourAmount - 5;
-                        count5++;
-                    }
+            new Handler().postDelayed(() -> {
+                imageView2.setImageResource(R.drawable.male_avatar);
+                textView2.setText("Yes ma'am! wait a minute.");
+            },1000);
+            long flourAmount = Long.parseLong(amount),count3 = 0,count5 = 0;
+            while (flourAmount>=5 || flourAmount == 3){
+                if(flourAmount == 3 || flourAmount == 6 || flourAmount == 9){
+                    flourAmount = flourAmount-3;
+                    count3++;
+                }else{
+                    flourAmount = flourAmount - 5;
+                    count5++;
                 }
-                int finalFlourAmount = flourAmount;
-                String count_3 = String.valueOf(count3),count_5 = String.valueOf(count5);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageView3.setImageResource(R.drawable.male_avatar);
-                        if(finalFlourAmount <3){
-                            textView3.setText("There you have your "+amount+" kg of wheat flour packed in "+count_5+" 5Kg bags and "+count_3+" 3kg bags.");
-                        }else {
-                            textView3.setText("Sorry we cant put the amount you want into 5 and 3kg bags.");
-                        }
-                    }
-                },2000);
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageView4.setImageResource(R.drawable.female_avatar);
-                        textView4.setText("Thankyou");
-                    }
-                },3000);
             }
+            Long finalFlourAmount = flourAmount;
+            String count_3 = String.valueOf(count3),count_5 = String.valueOf(count5);
+            new Handler().postDelayed(() -> {
+                imageView3.setImageResource(R.drawable.male_avatar);
+                if(finalFlourAmount == 0){
+                    textView3.setText("There you have your "+amount+" kg of wheat flour packed in "+count_5+" 5Kg bags and "+count_3+" 3kg bags.");
+                }else {
+                    textView3.setText("Sorry we cant put the amount you want into 5 and 3kg bags.");
+                }
+            },2000);
+
+            new Handler().postDelayed(() -> {
+                imageView4.setImageResource(R.drawable.female_avatar);
+                textView4.setText("Thankyou");
+            },3000);
         });
 
     }
